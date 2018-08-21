268f70479ffe8986ac61cd2fe371ffbaf7bdbbb6
Unknown <vero@nuagesdencre.com>
2018-08-21 12:18:30 +0100

    building up the framework of the project

---
+++ run.py
@@ -0,0 +1,15 @@
+import os
+import json
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html", page_title="Home")
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
