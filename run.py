14863ed1fb0f2565e77e4b789c94d7632accb022
Unknown <vero@nuagesdencre.com>
2018-08-24 09:02:43 +0100

    project boilerplate completed

--- run.py
+++ run.py
@@ -8,6 +8,18 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", page_title="Home")
 
+@app.route('/about')
+def about():
+    return render_template("about.html", page_title="About")
+
+@app.route('/game')
+def game():
+    return render_template("game.html", page_title="Game")
+    
+@app.route('/contact')
+def contact():
+    return render_template("contact.html",page_title="Contact")
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
