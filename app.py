5592c90dd42177215bdc884d8ba797a506593eca
Unknown <vero@nuagesdencre.com>
2018-09-19 13:12:07 +0100

    adder username placeholder, added write_to_file func

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+
 app = Flask(__name__)
+
+
+def write_to_file(filename, data):
+    with open(filename, "a") as file:
+        file.writelines(data)
 
 
 @app.route('/')
 def index():
-    return render_template("index.html", page_title="Home")
+    username = 'Visitor'
+    return render_template("index.html", page_title="Home", username=username)
 
 
 @app.route('/about')
@@ -18,13 +25,17 @@ def about():
 def game():
     return render_template("game.html", page_title="Game")
 
+# include game, rules, leaderboard
+
+
 @app.route('/login')
 def login():
-    return render_template("login.html",page_title="Login")
+    return render_template("login.html", page_title="Login")
+
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html",page_title="Contact")
+    return render_template("contact.html", page_title="Contact")
 
 
 if __name__ == '__main__':
