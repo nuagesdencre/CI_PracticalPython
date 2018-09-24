d513eac37d9b76770d0cbbab1ed302d0fe28c247
Unknown <vero@nuagesdencre.com>
2018-09-24 16:30:47 +0100

    messing around with styles and animation...

--- static/js/main.js
+++ static/js/main.js
@@ -1,30 +1,23 @@
-$(function() {
-// toggle all paragraphs?
-    $("#paragraph_one").delay(9999).fadeOut(2500,function(){
-    $("#paragraph_two").slideDown(10000,function(){
-    $("#paragraph_two").fadeOut(2500,function(){
-    $("#riddle").slideDown(10000,function(){
-    $(".next_action").fadeIn(2300,function(){
-    $(".btns").fadeIn(2500) }) })}) })
-  });
-
-     $('#btn_p_one').click(function () {
-        $('#paragraph_one').toggle()
-        $('#paragraph_two').hide()
-        $('#riddle').hide();
-    });
-
-    $('#btn_p_two').click(function () {
-        $('#paragraph_one').hide()
-        $('#paragraph_two').toggle();
-        $('#riddle').hide();
-    });
-
-    $('#btn_riddle').click(function () {
-        $('#paragraph_one').hide(2250)
-        $('#paragraph_two').hide(2250);
-        $('#riddle').toggle(2250);
-    });
-})
+// $(function() {
+// // toggle all paragraphs?
+//
+//       $('#btn_p_one').click(function () {
+//         $('#paragraph_one').toggle()
+//         $('#paragraph_two').hide()
+//         $('#riddle').hide();
+//     });
+//
+//     $('#btn_p_two').click(function () {
+//         $('#paragraph_one').hide()
+//         $('#paragraph_two').toggle();
+//         $('#riddle').hide();
+//     });
+//
+//     $('#btn_riddle').click(function () {
+//         $('#paragraph_one').hide(2250)
+//         $('#paragraph_two').hide(2250);
+//         $('#riddle').toggle(2250);
+//     });
+// })
 
 
