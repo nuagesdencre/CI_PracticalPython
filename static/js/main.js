99ed7e169ed1046b25f8e29406da54a53b2c7117
Unknown <vero@nuagesdencre.com>
2018-09-25 13:34:20 +0100

    simplifying things up

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,7 @@
-// $(function() {
-// // toggle all paragraphs?
+// $(function () {
 //
-//       $('#btn_p_one').click(function () {
-//         $('#paragraph_one').toggle()
-//         $('#paragraph_two').hide()
-//         $('#riddle').hide();
-//     });
-//
-//     $('#btn_p_two').click(function () {
-//         $('#paragraph_one').hide()
-//         $('#paragraph_two').toggle();
-//         $('#riddle').hide();
-//     });
-//
-//     $('#btn_riddle').click(function () {
-//         $('#paragraph_one').hide(2250)
-//         $('#paragraph_two').hide(2250);
-//         $('#riddle').toggle(2250);
-//     });
 // })
 
-
+$('#btn_riddle').click(function () {
+        $('.riddle').toggle();
+    });
