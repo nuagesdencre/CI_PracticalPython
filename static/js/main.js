df0f069b125e72459ae987081f6b0378969dc852
Unknown <vero@nuagesdencre.com>
2018-09-28 17:41:56 +0100

    reorganising code, added a highlight on leaderboard page

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,45 @@
-// $(function () {
-//
-// })
+$(function () {
 
-$('#btn_riddle').click(function () {
+    // buttons revealing clues and red herrings
+    $('#btn_riddle').click(function () {
         $('.riddle').toggle();
     });
+
+    $('#btn_painting_couple').click(function () {
+        $('.painting_couple').toggle();
+    });
+
+    $('#btn_room').click(function () {
+        $('.room_view').toggle();
+    });
+
+    $('#btn_clock').click(function () {
+        $('.clock_view').toggle();
+    });
+
+    $('#btn_bookshelf').click(function () {
+        $('.bookshelf').toggle();
+    });
+
+    $('#btn_paintings').click(function () {
+        $('.paintings').toggle();
+    });
+
+    $('#btn_chessboard').click(function () {
+        $('.chessboard').toggle();
+    });
+
+    $('#btn_paintings').click(function () {
+        $('.paintings_view').toggle();
+    });
+
+    // highlighting the player's own stats in the leaderboard
+    function highlight_leaderboard() {
+        if ($('#leaderboard_check').html() != null && $('#leaderboard_check').html() != 'Potato') {
+            return $('#player').addClass('leaderboard_top');
+        }
+        $('#player').removeClass('leaderboard_top');
+    }
+
+    highlight_leaderboard();
+})
