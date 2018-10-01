9c0102f2d44623fde99f19031adf32d73a3e4bd1
Unknown <vero@nuagesdencre.com>
2018-10-01 12:16:55 +0100

    organizing main.js for clarity

--- static/js/main.js
+++ static/js/main.js
@@ -1,32 +1,46 @@
 $(function () {
 
-    // buttons revealing clues and red herrings
-    $('#btn_riddle').click(function () {
-        $('.riddle').toggle();
-    });
-        $('#btn_dining').click(function () {
+    /* buttons revealing clues and red herrings*/
+    //part 1
+
+     $('#btn_dining').click(function () {
         $('.dining').toggle();
     });
     $('#btn_tablecloth').click(function () {
         $('.tablecloth').toggle();
     });
-    $('#btn_painting_couple').click(function () {
+
+    $('#btn_room').click(function () {
+        $('.room_view').toggle();
+    });
+
+    //part 2
+     $('#btn_painting_couple').click(function () {
         $('.painting_couple').toggle();
     });
 
     $('#btn_painting_check').click(function () {
         $('.painting_check').toggle();
     });
-    $('#btn_room').click(function () {
-        $('.room_view').toggle();
-    });
 
     $('#btn_clock').click(function () {
         $('.clock_view').toggle();
     });
+    $('#btn_clock_inside').click(function () {
+        $('.clock_inside').toggle();
+    });
+
+    //part 3
 
     $('#btn_bookshelf').click(function () {
         $('.bookshelf').toggle();
+    });
+
+        $('#btn_under_the_bed').click(function () {
+        $('.under_the_bed').toggle();
+    });
+    $('#btn_chest').click(function () {
+        $('.chest').toggle();
     });
 
     $('#btn_paintings').click(function () {
@@ -37,10 +51,21 @@ $(function () {
         $('.chessboard').toggle();
     });
 
+        $('#btn_missing').click(function () {
+        $('.missing').toggle();
+    });
+
     $('#btn_paintings').click(function () {
         $('.paintings_view').toggle();
     });
 
+    //riddle
+    $('#btn_riddle').click(function () {
+        $('.riddle').toggle();
+    });
+   /* END buttons revealing clues and red herrings*/
+
+    // LEADERBOARD
     // highlighting the player's own stats in the leaderboard
     function highlight_leaderboard() {
         if ($('#leaderboard_check').html() != null && $('#leaderboard_check').html() != 'Potato') {
