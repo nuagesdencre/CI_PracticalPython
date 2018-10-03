fdad123b5995399e61c90ba41c61b279ea3e4e4e
Unknown <vero@nuagesdencre.com>
2018-10-03 12:40:05 +0100

    refactoring code btns - toggle

--- static/js/main.js
+++ static/js/main.js
@@ -1,84 +1,12 @@
 $(function () {
 
     /* buttons revealing clues and red herrings*/
-    //part 1
 
-     $('#btn_dining').click(function () {
-        $('.dining').toggle();
-        $('#btn_dining').toggleClass('lace');
+    $('.btns').click(function () {
+        let selector = $(this).data('toggle');
+        $(selector).toggle();
+        $(this).toggleClass('lace');
     });
-    $('#btn_tablecloth').click(function () {
-        $('.tablecloth').toggle();
-        $('#btn_tablecloth').toggleClass('lace');
-    });
-
-    $('#btn_room').click(function () {
-        $('.room_view').toggle();
-        $('#btn_room').toggleClass('lace');
-    });
-
-    //part 2
-     $('#btn_picture_couple').click(function () {
-        $('.picture_couple').toggle();
-        $('#btn_picture_couple').toggleClass('lace');
-    });
-
-    $('#btn_painting_check').click(function () {
-        $('.painting_check').toggle();
-        $('#btn_painting_check').toggleClass('lace');
-    });
-
-    $('#btn_clock').click(function () {
-        $('.clock_view').toggle();
-        $('#btn_clock').toggleClass('lace');
-    });
-    $('#btn_clock_inside').click(function () {
-        $('.clock_inside').toggle();
-        $('#btn_clock_inside').toggleClass('lace');
-    });
-
-    //part 3
-
-    $('#btn_bookshelf').click(function () {
-        $('.bookshelf').toggle();
-        $('#btn_bookshelf').toggleClass('lace');
-    });
-
-        $('#btn_under_the_bed').click(function () {
-        $('.under_the_bed').toggle();
-         $('#btn_under_the_bed').toggleClass('lace');
-    });
-    $('#btn_chest').click(function () {
-        $('.chest').toggle();
-        $('#btn_chest').toggleClass('lace');
-    });
-
-    $('#btn_paintings').click(function () {
-        $('.paintings').toggle();
-        $('#btn_paintings').toggleClass('lace');
-    });
-
-    $('#btn_chessboard').click(function () {
-        $('.chessboard').toggle();
-        $('#btn_chessboard').toggleClass('lace');
-    });
-
-        $('#btn_missing').click(function () {
-        $('.missing').toggle();
-        $('#btn_missing').toggleClass('lace');
-    });
-
-    $('#btn_paintings').click(function () {
-        $('.paintings_view').toggle();
-        $('#btn_paintings_view').toggleClass('lace');
-    });
-
-    //riddle
-    $('#btn_riddle').click(function () {
-        $('.riddle').toggle();
-        $('#btn_riddle').toggleClass('lace');
-    });
-   /* END buttons revealing clues and red herrings*/
 
     // LEADERBOARD
     // highlighting the player's own stats in the leaderboard
