3d34982726b0b2e8ec30e26e688b21136aa2ef84
Unknown <vero@nuagesdencre.com>
2018-10-03 13:20:17 +0100

    adding an alert if the reader has not started an instance yet

--- static/js/main.js
+++ static/js/main.js
@@ -11,11 +11,11 @@ $(function () {
     // LEADERBOARD
     // highlighting the player's own stats in the leaderboard
     function highlight_leaderboard() {
-        if ($('#leaderboard_check').html() != null && $('#leaderboard_check').html() != 'Potato') {
+        if ($('#leaderboard_check').html() != "   " && $('#leaderboard_check').html() != "Potato") {
             return $('#player').addClass('leaderboard_top');
         }
         $('#player').removeClass('leaderboard_top');
+        alert("Your progress is not being recorded yet, begin the story and introduce yourself.")
     }
-
     highlight_leaderboard();
 })
