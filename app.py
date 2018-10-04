804427b0b8f2b940399d2e03f769d319715931fe
v s <vero@nuagesdencre.com>
2018-10-04 18:19:24 +0100

    heroku deployment contd

--- app.py
+++ app.py
@@ -128,5 +128,5 @@ def page_not_found(err):
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0',
+            port=int(os.environ.get('PORT', 5000))
